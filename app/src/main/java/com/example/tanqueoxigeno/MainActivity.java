909a6760abf4a7a209a9e543445c827e9d232f18
1999jsgh <72871194+1999jsgh@users.noreply.github.com>
2022-04-29 22:44:27 -0500

    implementacion de interfaces de cliente de iniciar el login

--- app/src/main/java/com/example/tanqueoxigeno/MainActivity.java
+++ app/src/main/java/com/example/tanqueoxigeno/MainActivity.java
@@ -2,7 +2,23 @@ package com.example.tanqueoxigeno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b_registros = findViewById(R.id.botonRegistar);
+        EditText e_correo = findViewById(R.id.editCorreo);
+        EditText e_contra = findViewById(R.id.editContra);
+
+        b_registros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            obtenerDatos(e_correo.getText().toString(),e_contra.getText().toString());
+            }
+        });
+
+    }
+
+    private void obtenerDatos(String user, String pass) {
+
+        Retrofit retrofit= new Retrofit.Builder()
+                .baseUrl("http://localhost/Botellasgas/cliente/edit.php")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JSONObject jsonObject= new JSONObject();
+        try {
+            jsonObject.put("user", user);
+            jsonObject.put("contrasena", pass);
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsonObject.toString());
+        Log.e("", "------>" + jsonObject.toString());
+
     }
 }
---
+++ app/src/main/java/com/example/tanqueoxigeno/cliente/Cliente.java
@@ -0,0 +1,90 @@
+package com.example.tanqueoxigeno.cliente;
+
+public class Cliente {
+    private int cliente_id;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private String contrasena;
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "cliente_id=" + cliente_id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correo='" + correo + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                '}';
+    }
+}
